850a5a50966d66a1a74652372aa6ae59c7c20673
yunik1004 <inkyuhak@gmail.com>
2018-12-26 13:20:12 +0900

    Add socket.io

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,9 @@ const app = new Koa()
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-export default app
+const server = require('http').createServer(app.callback())
+const io = require('socket.io')(server)
+
+server.listen(8080, () => {
+  console.log('server is listening to port 8080')
+})
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from './app'
-
-app.listen(8080, () => {
-  console.log('server is listening to port 8080')
-})
